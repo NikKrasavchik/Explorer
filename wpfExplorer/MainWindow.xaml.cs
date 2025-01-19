9fd68fdf090642143693245ecb08df7a123068e7
NikKrasavchik <stachinskiy.nikita@gmail.com>
2025-01-20 01:24:01 +0300

    123

--- wpfExplorer/MainWindow.xaml.cs
+++ wpfExplorer/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
-﻿// MainWindow.xaml.cs
+﻿using MahApps.Metro.Controls;
 using libraryExplorer;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace wpfExplorer
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private FileManager _fileManager;
 
@@ -18,7 +20,7 @@ namespace wpfExplorer
 
         private void LoadRootDirectories()
         {
-            foreach (var drive in System.IO.DriveInfo.GetDrives())
+            foreach (var drive in DriveInfo.GetDrives())
             {
                 TreeViewItem item = new TreeViewItem() { Header = drive.Name, Tag = drive.Name };
                 item.Items.Add(null); // Заглушка для возможности раскрытия узла
@@ -32,14 +34,37 @@ namespace wpfExplorer
             {
                 string path = selectedItem.Tag.ToString();
                 selectedItem.Items.Clear(); // Очистка заглушки
-                foreach (var dir in _fileManager.GetDirectoryContents(path))
+                foreach (var dir in Directory.GetDirectories(path))
                 {
-                    TreeViewItem subItem = new TreeViewItem() { Header = System.IO.Path.GetFileName(dir), Tag = dir };
+                    TreeViewItem subItem = new TreeViewItem() { Header = Path.GetFileName(dir), Tag = dir };
                     subItem.Items.Add(null); // Добавление заглушки для возможности раскрытия узла
                     selectedItem.Items.Add(subItem);
                 }
-                FileList.ItemsSource = _fileManager.GetDirectoryContents(path);
+                FileList.ItemsSource = GetFileList(path);
             }
         }
+
+        private List<FileItem> GetFileList(string path)
+        {
+            List<FileItem> fileItems = new List<FileItem>();
+            foreach (var filePath in Directory.GetFiles(path))
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                fileItems.Add(new FileItem
+                {
+                    Name = fileInfo.Name,
+                    Type = fileInfo.Extension,
+                    Size = fileInfo.Length
+                });
+            }
+            return fileItems;
+        }
+    }
+
+    public class FileItem
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public long Size { get; set; }
     }
 }
